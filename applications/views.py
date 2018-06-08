1b7a1605f11a2ad85a4172fa75924a435e3b516b
Saurav Kumar <saur.k10@gmail.com>
2018-06-09 02:30:08 +0530

    Adding a small authentication to see application details

--- applications/views.py
+++ applications/views.py
@@ -6,6 +6,20 @@ from utils.generic import generate_applicationid, create_application, AppDetails
 
 # Create your views here.
 
+class ApplicationAuthMixin(View):
+    """
+    Show application details to the user who applies it. Though there can be an exception for admins. :)
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        applicationid = kwargs.get('applicationid')
+        cookie_apps = request.COOKIES.get('applications') or ''
+        # ToDo: Add an exception for Admin as admin can see this application details page.
+        if applicationid not in cookie_apps:
+            return HttpResponseRedirect(reverse('apply'))
+        return super(ApplicationAuthMixin, self).dispatch(request, *args, **kwargs)
+
+
 class LoginView(View):
     # ToDo: create a login form and the a login template
     pass
@@ -28,12 +42,18 @@ class ApplyView(View):
         app_data = form.cleaned_data
         app_data['applicationid'] = applicationid
         create_application(app_data)
-        return HttpResponseRedirect(reverse('application', kwargs={'applicationid': applicationid}))
+        # Though the application id is a very unique and very difficult to guess,
+        # it's good to add the applicationid in the current user active session.
+        cookie_apps = request.COOKIES.get('applications') or ''
+        cookie_apps += ',' + applicationid
+        response = HttpResponseRedirect(reverse('application', kwargs={'applicationid': applicationid}))
+        response.set_cookie('applications', cookie_apps)
+        return response
 
 
-class ApplicationView(View):
+class ApplicationView(ApplicationAuthMixin, View):
     """
-    Show application
+    Show applied application to the user who applied it!
     """
 
     def get(self, request, *args, **kwargs):
