4bb03e249a0c8962632f6516638d5a0a3e10f990
Saurav Kumar <saur.k10@gmail.com>
2018-08-23 23:19:42 +0530

    Adding front-end form validation using AngularJS

--- applications/forms.py
+++ applications/forms.py
@@ -3,17 +3,17 @@ from django import forms
 class ApplicationForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=45, widget=forms.TextInput(
         attrs={
-            'id': 'txtFirstName', 'required': True, 'placeholder': 'First Name'
+            'id': 'txtFirstName', 'required': True, 'placeholder': 'First Name', 'ng-model': 'txtFirstName'
         }
     ))
     last_name = forms.CharField(label='Last Name', max_length=45, widget=forms.TextInput(
         attrs={
-            'id': 'txtLastName', 'required': True, 'placeholder': 'Last Name'
+            'id': 'txtLastName', 'required': True, 'placeholder': 'Last Name', 'ng-model': 'txtLastName'
         }
     ))
     email = forms.EmailField(label='Email', max_length=50, widget=forms.EmailInput(
         attrs={
-            'id': 'txtEmail', 'required': True, 'placeholder': 'Email'
+            'id': 'txtEmail', 'required': True, 'placeholder': 'Email', 'ng-model': 'txtEmail'
         }
     ))
     # ToDo: add few more application fields
